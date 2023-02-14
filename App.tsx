df6dc3f16228e2cbe6271bbff91d01acfb4412db
JohnFord9159 <johnford9159@gmail.com>
2023-02-14 04:24:18 -0800

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './style.scss';
-import { ComponentCreator } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 function Header() {
   return (
